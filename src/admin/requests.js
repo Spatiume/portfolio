18f19ae7ecf2747b0666a7f6230d96e79f3c472d
spatium <sysoevpvl@gmail.com>
2022-08-17 14:54:55 +0300

    added refresh token in admin panel

--- src/admin/requests.js
+++ src/admin/requests.js
@@ -1,17 +1,36 @@
 import axios from "axios";
 
-const token = localStorage.getItem('token');
+const token = localStorage.getItem("token");
 
-if (!token) {
-  console.warn("Токен отсутствует");
-}
+if (!token) { console.warn("Отсутствует токен"); }
 
-const $axios = axios.create({
+const requests = axios.create({
   baseURL: "https://webdev-api.loftschool.com",
   headers: {
-    "Authorization": `Bearer ${token}`
+    Authorization: `Bearer ${token}`
   }
+});
 
-})
+requests.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
 
-export default $axios;
+    if (error.response.status === 401) { // 401 ошибку возвращает Api когда токен истёк
+      const response = await requests.post("/refreshToken"); // получаем новый токен
+      const token = response.data.token;
+
+      localStorage.setItem("token", token); //устанавливаем токен в локалсторадж
+      requests.defaults.headers["Authorization"] = `Bearer ${token}`; //меняем токен в заголовке
+      originalRequest.headers["Authorization"] = `Bearer ${token}`;
+      // меняем токен в заголовке в запросе, в котором пришла ошибка
+
+      //возвращаем данные с запросом и вызываем его повторно
+      return axios(originalRequest);
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default requests;
